c007eeab068015dbc4d991d3322b1dd4ded78841
HG <h.gail@web.de>
2023-07-17 15:02:34 +0200

    IO monotonic start etc.

--- modules/core/src/test/scala/TestTry.scala
+++ modules/core/src/test/scala/TestTry.scala
@@ -1,25 +1,31 @@
 import cats.effect.implicits.genSpawnOps
 import cats.effect.{ExitCode, IO, IOApp, Resource}
+import cats.implicits.toFunctorOps
 import fs2.Stream
 
 import scala.concurrent.duration._
 
 object TestTry extends IOApp {
   override def run(args: List[String]): IO[ExitCode] = {
-    def job = IO.monotonic <* IO(println("do JOb ")) <* IO.sleep(1.second)
+    def job(n : Int) = IO.monotonic  <* IO.sleep(6.second) <* IO(println("do JOb "+n))
 
     def producer =
-      Stream(job)
-        .repeatN(10L)
+      Stream(job(_))
+        .repeatN(5L)
         .covary[IO]
-        .meteredStartImmediately(0.5.seconds)
-        .evalMap( job =>   job    )
+        .meteredStartImmediately(6.seconds)
+        .zipWithIndex
+        .evalTap( j => IO(println("emit  "+j))    )
+        .evalMap( job =>   job._1(job._2.toInt)    )
         .compile
-        .toVector
+        .drain
 
-    val res = Resource.make[IO, FiniteDuration](IO.monotonic <* IO(println("Satrt ")))(_ => IO(println("End ")))
+    val res = Resource.make[IO, FiniteDuration](job(-1))(_ => IO(println("End ")))
 
-      res.background.surround[Vector[FiniteDuration]](producer )
+     // res.surround(producer )
+    //todo job(-10) -> res.use ( job
+    job(-10).foreverM.background.surround(producer ).as(ExitCode.Success)
+    //job(-10).background.use( d =>  d).as(ExitCode.Success)
 
   }
 }
